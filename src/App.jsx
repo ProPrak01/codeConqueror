082c3820511c67d5fd7723fca2b531d877881404
KaranSingh52ED <ed22b052@smail.iitm.ac.in>
2024-04-09 15:45:19 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
 import './App.css'
-import {Article,Brand,CTA,Navbar} from '../components'
-import {Footer, Header,Possibility,WhatGPT3,Features,Blog} from '../containers'
+import { Article, Brand, CTA, Navbar } from '../components'
+import { Footer, Header, Possibility, Features, Blog } from '../containers'
 function App() {
 
 
   return (
     <div className='App'>
-    <div className='gradient_bg'>
-      <Navbar/>
-      <Header/>
+      <div className='gradient_bg'>
+        <Navbar />
+        <Header />
 
 
-    </div>
-    <Brand/>
-    <WhatGPT3/>
-    <Features/>
-    <Possibility/>
-    <CTA/>
-    <Blog/>
-    <Footer/>
-   
+      </div>
+      <Brand />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+
     </div>
   )
 }
