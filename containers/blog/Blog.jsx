bf35ea41286d5596d77236280069cb4913a95986
KaranSingh52ED <ed22b052@smail.iitm.ac.in>
2024-04-09 18:43:04 +0530

    changes

--- containers/blog/Blog.jsx
+++ containers/blog/Blog.jsx
@@ -1,61 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Blog.css'
+import Loading from '../../components/Loader/Loader';
 const Blog = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch(error => console.error('Error:', error));
+  }, [])
+  console.log(data);
   return (
-    <div className='box-container'>
-<div className="box">
-  <div className="box-icon"><img height={42}  src="../../assets/interiit.jpg" /></div>
-  <div className="box-label">PHOTO 1</div>
-  <div className="box-title">Placid.app Editor Basics ✨</div>
-  <div className="box-image">
-    <img src="../../assets/ai.png" alt />
-  </div>
-  <div className="studio-button">
-    <div className="studio-button-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><circle cx={12} cy={12} r={10} /></svg>
-    </div>
-    <div className="studio-button-label">
-      Open
-    </div>
-  </div>
-</div>
-<div className="box">
-  <div className="box-icon"><svg xmlns="http://www.w3.org/2000/svg" width={42} height={42} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round"><path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z" /></svg></div>
-  <div className="box-label">PHOTO 2</div>
-  <div className="box-title">Placid.app Editor Basics ✨</div>
-  <div className="box-image">
-    <img src="../../assets/ai.png" alt />
-  </div>
-  <div className="studio-button">
-    <div className="studio-button-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><circle cx={12} cy={12} r={10} /></svg>
-    </div>
-    <div className="studio-button-label">
-      Open
-    </div>
-  </div>
-</div>
-<div className="box">
-  <div className="box-icon"><svg xmlns="http://www.w3.org/2000/svg" width={42} height={42} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1} strokeLinecap="round" strokeLinejoin="round"><path d="M18 3a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3H6a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3V6a3 3 0 0 0-3-3 3 3 0 0 0-3 3 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 3 3 0 0 0-3-3z" /></svg></div>
-  <div className="box-label">PHOTO 3</div>
-  <div className="box-title">Placid.app Editor Basics ✨</div>
-  <div className="box-image">
-    <img src="../../assets/ai.png" alt />
-  </div>
-  <div className="studio-button">
-    <div className="studio-button-icon">
-      <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><circle cx={12} cy={12} r={10} /></svg>
-    </div>
-    <div className="studio-button-label">
-      Open
-    </div>
-  </div>
-</div>
+    <div>
+      {< div className='box-container'>
+        data ? ({
 
+          data.map((item) => (
 
+            <div className="box">
+              <div className="box-icon"><img height={42} src="../../assets/interiit.jpg" /></div>
+              <div className="box-label">{item.id} </div>
+              <div className="box-title">
 
-    </div>
- 
+                ✨</div>
+              <div className="box-image">
+               
+                <img src={item.url} alt="image" />
+              </div>
+              <div className="studio-button">
+                <div className="studio-button-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width={22} height={22} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><circle cx={12} cy={12} r={10} /></svg>
+                </div>
+                <div className="studio-button-label">
+                  Open
+                </div>
+              </div>
+            </div>
+
+          ))
+
+        }
+        )
+
+        : (
+        <Loading />
+        )
+      </div>
+      }
+    </div >
+
   )
 }
 
